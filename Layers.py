Fixed basic backpropagation logic, added Embedding layer + updated ADAM logic accordingly

Layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Tokenizer import tokenizer
 
 class Layer:
     def __init__(self, *args):
@@ -21,25 +22,48 @@ class Dense(Layer):
     
     def forward(self, input):
         self.input = input
-        self.z = self.w @ self.input + self.b
+        self.z = self.input @ self.w + self.b
         self.output = self.activation.forward(self.z)
         return self.output
     
     def backward(self, w_next, delta_next, regularization):
-        self.delta = w_next.T @ delta_next * self.activation.backward()
-        self.dw = self.delta @ self.input.T + regularization.backward(self.w)
-        self.db = np.sum(self.delta, axis=1, keepdims=True)
+        self.delta = delta_next @ w_next.T * self.activation.backward()
+        self.dw = self.input.T @ self.delta + regularization.backward(self.w)
+        self.db = np.sum(self.delta, axis=0, keepdims=True)
         return self.dw, self.db, self.delta, self.w
+
+class Embedding(Layer):
+    def __init__(self, vocabulary_size, embedding_depth):
+        self.vocabulary_size = vocabulary_size
+        self.embedding_depth = embedding_depth
+        self.embedding_matrix = np.random.rand(self.vocabulary_size, self.embedding_depth)
+        
+    def forward(self, ID_input):
+        self.ID_input = ID_input
+        self.embedded = np.zeros((1, self.embedding_depth))
+        
+        for token in self.ID_input:
+            self.embedded = np.concatenate((self.embedded, self.embedding_matrix[token:token+1, :]), axis=0)
+            
+        return self.embedded[1:, :]
+        
+    def backward(self, w_next, delta_next, regularization):
+        self.gradient_matrix = np.zeros_like(self.embedding_matrix)
+        
+        for i, token in enumerate (self.ID_input):
+            self.embedded[token] += delta_next[i]               # CHECK LOGIC OF ADDING DELTA TO WHOLE ROW
+            
+        return self.gradient_matrix
 
 class Xavier(Initialisation):
     def init (self, type, inputs, neurons):
         'Type: uniform/normal'
         b = np.zeros((neurons, 1))
         if type == 'uniform':
-            w = (np.random.rand(neurons, inputs) - 0.5) * 2 * np.sqrt(6 / (inputs + neurons))
+            w = (np.random.rand(inputs, neurons) - 0.5) * 2 * np.sqrt(6 / (inputs + neurons))
 
         elif type == 'normal':
-            w = (np.random.randn(neurons, inputs)) * np.sqrt(2 / (inputs + neurons))
+            w = (np.random.randn(inputs, neurons)) * np.sqrt(2 / (inputs + neurons))
 
         else:
             print('Choose viable Initialisation type')
@@ -52,12 +76,12 @@ class He(Initialisation):
         self.neurons = neurons
 
         'Type: uniform/normal'
-        b = np.zeros((neurons, 1))
+        b = np.zeros((1, neurons))
         if type == 'uniform':
-            w = (np.random.rand(neurons, inputs) - 0.5) * 2 * np.sqrt(6 / inputs)
+            w = (np.random.rand(inputs, neurons) - 0.5) * 2 * np.sqrt(6 / inputs)
 
         elif type == 'normal':
-            w = (np.random.randn(neurons, inputs)) * np.sqrt(2 / inputs)
+            w = (np.random.randn(inputs, neurons)) * np.sqrt(2 / inputs)
 
         else:
             print('Choose viable Initialisation type')
@@ -66,6 +90,6 @@ class He(Initialisation):
     
 class Random(Initialisation):
     def init(self, inputs, neurons):
-        w = (np.random.rand(neurons, inputs) - 0.5) * 2
-        b = (np.random.rand(neurons, 1) - 0.5) * 2
+        w = (np.random.rand(inputs, neurons) - 0.5) * 2
+        b = (np.random.rand(1, neurons) - 0.5) * 2
         return w, b 

Network.py
@@ -22,7 +22,7 @@ class Neural_network:
         self.delta = self.loss_f.backward()
         for i in range(len(self.layers) - 1, -1, -1):
             if i == len(self.layers) - 1:
-                self.layers[i].backward(np.ones((self.delta.shape[0], self.delta.shape[0])), self.delta, self.regularization)
+                self.layers[i].backward(np.ones((self.delta.shape[1], self.delta.shape[1])), self.delta, self.regularization)
                 
             else:
                 self.layers[i].backward(self.layers[i+1].w, self.layers[i+1].delta, self.regularization)

Optimization.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Layers import Embedding
 
 class ADAM:
     def __init__(self, alfa, beta1=0.9, beta2=0.999, epsilon=1e-9):
@@ -13,31 +14,51 @@ class ADAM:
         self.v_b = []
 
     def optimize(self, layers):
-        for i, layer in enumerate(layers):
-            if self.t == 0:
-                self.m_w.append(np.zeros_like(layer.w))
-                self.v_w.append(np.zeros_like(layer.w))
-                self.m_b.append(np.zeros_like(layer.b))
-                self.v_b.append(np.zeros_like(layer.b))
-
-            self.m_w[i] = self.beta1 * self.m_w[i] + (1 - self.beta1) * layer.dw
-            self.v_w[i] = self.beta2 * self.v_w[i] + (1 - self.beta2) * (layer.dw ** 2)
-            self.m_b[i] = self.beta1 * self.m_b[i] + (1 - self.beta1) * layer.db
-            self.v_b[i] = self.beta2 * self.v_b[i] + (1 - self.beta2) * (layer.db ** 2)
-
-            if self.t > 0:
-                self.m_w_cor = self.m_w[i] / (1 - self.beta1 ** self.t)
-                self.v_w_cor = self.v_w[i] / (1 - self.beta2 ** self.t)
-                self.m_b_cor = self.m_b[i] / (1 - self.beta1 ** self.t)
-                self.v_b_cor = self.v_b[i] / (1 - self.beta2 ** self.t)
+        i = 0
+        for layer in layers:
+            if isinstance(layer, Embedding):
+                if self.t == 0:
+                    self.m = np.zeros_like(layer.embedding_matrix)
+                    self.v = np.zeros_like(layer.embedding_matrix)
+                    
+                self.m = self.beta1 * self.m + (1 - self.beta1) * layer.gradient_matrix
+                self.v = self.beta2 * self.v + (1 - self.beta2) * (layer.gradient_matrix ** 2)
+                
+                if self.t > 0:
+                    self.m_cor = self.m / (1 - self.beta1 ** self.t)
+                    self.v_cor = self.v / (1 - self.beta2 ** self.t)
+                else:
+                    self.m_cor = self.m
+                    self.v_cor = self.v
+                layer.embedding_matrix -= self.alfa * self.m_cor / (np.sqrt(self.v_cor) + self.epsilon)
+                
             else:
-                self.m_w_cor = self.m_w[i]
-                self.v_w_cor = self.v_w[i]
-                self.m_b_cor = self.m_b[i]
-                self.v_b_cor = self.v_b[i]
-
-            layer.w -= self.alfa * self.m_w_cor / (np.sqrt(self.v_w_cor) + self.epsilon)
-            layer.b -= self.alfa * self.m_b_cor / (np.sqrt(self.v_b_cor) + self.epsilon)
+                if self.t == 0:
+                    self.m_w.append(np.zeros_like(layer.w))
+                    self.v_w.append(np.zeros_like(layer.w))
+                    self.m_b.append(np.zeros_like(layer.b))
+                    self.v_b.append(np.zeros_like(layer.b))
+    
+                self.m_w[i] = self.beta1 * self.m_w[i] + (1 - self.beta1) * layer.dw
+                self.v_w[i] = self.beta2 * self.v_w[i] + (1 - self.beta2) * (layer.dw ** 2)
+                self.m_b[i] = self.beta1 * self.m_b[i] + (1 - self.beta1) * layer.db
+                self.v_b[i] = self.beta2 * self.v_b[i] + (1 - self.beta2) * (layer.db ** 2)
+    
+                if self.t > 0:
+                    self.m_w_cor = self.m_w[i] / (1 - self.beta1 ** self.t)
+                    self.v_w_cor = self.v_w[i] / (1 - self.beta2 ** self.t)
+                    self.m_b_cor = self.m_b[i] / (1 - self.beta1 ** self.t)
+                    self.v_b_cor = self.v_b[i] / (1 - self.beta2 ** self.t)
+                else:
+                    self.m_w_cor = self.m_w[i]
+                    self.v_w_cor = self.v_w[i]
+                    self.m_b_cor = self.m_b[i]
+                    self.v_b_cor = self.v_b[i]
+    
+                layer.w -= self.alfa * self.m_w_cor / (np.sqrt(self.v_w_cor) + self.epsilon)
+                layer.b -= self.alfa * self.m_b_cor / (np.sqrt(self.v_b_cor) + self.epsilon)
+                
+                i +=1
 
         self.t += 1
         

main.py
@@ -1,14 +1,16 @@
 import numpy as np
 from Losses import BCE, MSE, CCE
 from Activations import ReLU, sigmoid, SiLU, tanh
-from Layers import Dense, He, Random, Xavier
+from Layers import Dense, Embedding, He, Random, Xavier
 from Network import Neural_network, L2, Null
 from Optimization import ADAM, gradient_descent, LR_cosine_annealing, LR_decay, LR_exponential
-    
+from Tokenizer import tokenizer
+
 # --- Test loop ---
 alfa = 1e-2
-epochs = 100
-batch_size = 2
+epochs = 10000
+batch_size = 3
+
 # --- XOR problem ---
 dataset = np.array([[1, 1], [0, 1], [1, 0], [0, 0]])
 true = np.array([[0], [1], [1], [0]])
@@ -21,15 +23,26 @@ hidden_1 = Dense(2, 10, sigmoid(), He(), 'normal')
 hidden_2 = Dense(10, 10, sigmoid(), He(), 'normal')
 output_layer = Dense(10, 1, sigmoid(), He(), 'normal')
 
-nn = Neural_network(loss_f, optimizer, hidden_1, hidden_2, output_layer, regularization=L2(lambd=0.000001))
+nn = Neural_network(loss_f, optimizer, hidden_1, hidden_2, output_layer)
+
+# --- LM test loop ---
+lay_1 = Embedding(27, 16)
+lay_2 = Dense(16, 32, sigmoid(), He(), 'normal')
+nn_embed = Neural_network(loss_f, optimizer, lay_1, lay_2)
+tokenize = tokenizer()
+sentence = 'hello'
+
+ID = tokenize.encoder(sentence)
+prediction = nn_embed.forward_pass(ID)
+
 
 losses = []
 for epoch in range(epochs):
     total_loss = 0
 
     for l in range(0, dataset.shape[0], batch_size):
-        batch = dataset[(l):(l+batch_size), :].T
-        batch_true = true[(l):(l+batch_size), :].T
+        batch = dataset[(l):(l+batch_size), :]
+        batch_true = true[(l):(l+batch_size), :]
         x_pred = nn.forward_pass(batch)
 
         loss_value = nn.loss(x_pred, batch_true)
@@ -46,6 +59,6 @@ for epoch in range(epochs):
         print(f'Epoch: {epoch}, Loss: {total_loss}, LR: {optimizer.alfa}')
     
 # --- Results control ---
-final = nn.forward_pass(dataset.T)
-print(true.T)
+final = nn.forward_pass(dataset)
+print(true)
 print(final)